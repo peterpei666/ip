f2289cfeab46ef61c901d93eeaf9a14cfecab676
Pei Teer <mmiaomiao140@gmail.com>
2026-08-19 17:15:01 +0800

    Level-2: Add support for adding tasks and listing them

--- src/main/java/Peter.java
+++ src/main/java/Peter.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Peter {
@@ -10,24 +12,31 @@ public class Peter {
                     + "|  __/| |___  | | | |___|  _ < \n"
                     + "|_|   |_____| |_| |_____|_| \\_\\\n";
 
+        List<String> tasks = new ArrayList<>();
+
         System.out.println(logo);
         System.out.println("Hello! I'm Peter.");
         System.out.println("What can I do for you?");
         System.out.println(line);
-
 
         try (Scanner scanner = new Scanner(System.in)) {
             while (scanner.hasNextLine()) {
                 String input = scanner.nextLine();
                 System.out.println(line);
 
-                if (input.equals("bye")) {
+                if (input.equalsIgnoreCase("bye")) {
                     System.out.println("     Bye. Hope to see you again soon!");
                     System.out.println(line);
                     break;
+                } else if (input.equalsIgnoreCase("list")) {
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println("     " + (i + 1) + ". " + tasks.get(i));
+                    }
+                } else {
+                    tasks.add(input);
+                    System.out.println("     added: " + input);
                 }
-                
-                System.out.println("     " + input);
+
                 System.out.println(line);
             }
         }
