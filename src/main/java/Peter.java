9a766dbeaff15d9cbae748e8ccae70cf551c5770
Pei Teer <mmiaomiao140@gmail.com>
2026-08-19 17:19:23 +0800

    Level-3 & A-Classes: Add Task class with mark and unmark functionalities

--- src/main/java/Peter.java
+++ src/main/java/Peter.java
@@ -12,16 +12,16 @@ public class Peter {
                     + "|  __/| |___  | | | |___|  _ < \n"
                     + "|_|   |_____| |_| |_____|_| \\_\\\n";
 
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
 
         System.out.println(logo);
-        System.out.println("Hello! I'm Peter.");
-        System.out.println("What can I do for you?");
+        System.out.println(" Hello! I'm Peter.");
+        System.out.println(" What can I do for you?");
         System.out.println(line);
 
         try (Scanner scanner = new Scanner(System.in)) {
             while (scanner.hasNextLine()) {
-                String input = scanner.nextLine();
+                String input = scanner.nextLine().trim();
                 System.out.println(line);
 
                 if (input.equalsIgnoreCase("bye")) {
@@ -29,11 +29,29 @@ public class Peter {
                     System.out.println(line);
                     break;
                 } else if (input.equalsIgnoreCase("list")) {
+                    System.out.println("     Here are the tasks in your list:");
                     for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println("     " + (i + 1) + ". " + tasks.get(i));
+                        System.out.println("     " + (i + 1) + "." + tasks.get(i));
+                    }
+                } else if (input.startsWith("mark ")) {
+                    int taskIndex = Integer.parseInt(input.substring(5)) - 1;
+                    if (taskIndex >= 0 && taskIndex < tasks.size()) {
+                        Task task = tasks.get(taskIndex);
+                        task.markAsDone();
+                        System.out.println("     Nice! I've marked this task as done:");
+                        System.out.println("       " + task);
+                    }
+                } else if (input.startsWith("unmark ")) {
+                    int taskIndex = Integer.parseInt(input.substring(7)) - 1;
+                    if (taskIndex >= 0 && taskIndex < tasks.size()) {
+                        Task task = tasks.get(taskIndex);
+                        task.markAsUndone();
+                        System.out.println("     OK, I've marked this task as not done yet:");
+                        System.out.println("       " + task);
                     }
                 } else {
-                    tasks.add(input);
+                    Task newTask = new Task(input);
+                    tasks.add(newTask);
                     System.out.println("     added: " + input);
                 }
 
