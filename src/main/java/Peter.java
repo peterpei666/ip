12e0a14d24b2f264474206422ad705b9f5b2192f
Pei Teer <mmiaomiao140@gmail.com>
2026-08-19 17:10:03 +0800

    Level-1: Add support for echo and bye command

--- src/main/java/Peter.java
+++ src/main/java/Peter.java
@@ -1,18 +1,35 @@
+import java.util.Scanner;
+
 public class Peter {
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        String logo = " ____  _____ _____ _____ ____  \n"
+        String line = "    ____________________________________________________________";
+        String logo = "____________________________________________________________\n"
+                    + " ____  _____ _____ _____ ____  \n"
                     + "|  _ \\| ____|_   _| ____|  _ \\ \n"
                     + "| |_) |  _|   | | |  _| | |_) |\n"
                     + "|  __/| |___  | | | |___|  _ < \n"
                     + "|_|   |_____| |_| |_____|_| \\_\\\n";
 
-        System.out.println(line);
         System.out.println(logo);
         System.out.println("Hello! I'm Peter.");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                String input = scanner.nextLine();
+                System.out.println(line);
+
+                if (input.equals("bye")) {
+                    System.out.println("     Bye. Hope to see you again soon!");
+                    System.out.println(line);
+                    break;
+                }
+                
+                System.out.println("     " + input);
+                System.out.println(line);
+            }
+        }
     }
 }
