3ed8a2c0313386cc4d63d422a909011f5520cd08
Pei Teer <mmiaomiao140@gmail.com>
2026-08-26 19:54:06 +0800

    Add JUnit tests for Parser and TaskList classes (A-JUnit)

---
+++ src/test/java/peter/parser/ParserTest.java
@@ -0,0 +1,56 @@
+package peter.parser;
+
+import org.junit.jupiter.api.Test;
+import peter.exception.PeterException;
+import peter.task.Task;
+import peter.task.Todo;
+import peter.task.Deadline;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ParserTest {
+
+    @Test
+    public void parseTodo_validInput_success() throws PeterException {
+        Task task = Parser.parseTodo("todo read book");
+        assertTrue(task instanceof Todo);
+        assertEquals("[T][ ] read book", task.toString());
+    }
+
+    @Test
+    public void parseTodo_emptyDescription_exceptionThrown() {
+        PeterException exception = assertThrows(PeterException.class, () -> {
+            Parser.parseTodo("todo   ");
+        });
+        assertEquals("OOPS!!! The description of a todo cannot be empty.", exception.getMessage());
+    }
+
+    @Test
+    public void parseDeadline_validInput_success() throws PeterException {
+        Task task = Parser.parseDeadline("deadline return book /by Sunday");
+        assertTrue(task instanceof Deadline);
+        assertEquals("[D][ ] return book (by: Sunday)", task.toString());
+    }
+
+    @Test
+    public void parseDeadline_missingByFormat_exceptionThrown() {
+        assertThrows(PeterException.class, () -> {
+            Parser.parseDeadline("deadline return book Sunday");
+        });
+    }
+
+    @Test
+    public void parseIndex_validInteger_returnsZeroBasedIndex() throws PeterException {
+        int index = Parser.parseIndex("mark 2");
+        assertEquals(1, index);
+    }
+
+    @Test
+    public void parseIndex_invalidInteger_exceptionThrown() {
+        assertThrows(PeterException.class, () -> {
+            Parser.parseIndex("mark abc");
+        });
+    }
+}
---
+++ src/test/java/peter/task/TaskListTest.java
@@ -0,0 +1,45 @@
+package peter.task;
+
+import org.junit.jupiter.api.Test;
+import peter.exception.PeterException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TaskListTest {
+
+    @Test
+    public void delete_validIndex_taskRemovedSuccessfully() throws PeterException {
+        TaskList tasks = new TaskList();
+        Task todo = new Todo("read book");
+        tasks.add(todo);
+
+        Task removed = tasks.delete(0);
+        assertEquals(todo, removed);
+        assertEquals(0, tasks.size());
+    }
+
+    @Test
+    public void delete_invalidIndex_exceptionThrown() {
+        TaskList tasks = new TaskList();
+        tasks.add(new Todo("read book"));
+
+        assertThrows(PeterException.class, () -> {
+            tasks.delete(1);
+        });
+
+        assertThrows(PeterException.class, () -> {
+            tasks.delete(-1);
+        });
+    }
+
+    @Test
+    public void mark_validIndex_taskMarkedAsDone() throws PeterException {
+        TaskList tasks = new TaskList();
+        Task todo = new Todo("read book");
+        tasks.add(todo);
+
+        Task marked = tasks.mark(0);
+        assertEquals("[T][X] read book", marked.toString());
+    }
+}
